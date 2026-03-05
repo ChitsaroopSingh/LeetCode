0d73fe2147d3e84d4e71c519200333e0144bc57a
ZeuS <69910617+ChitsaroopSingh@users.noreply.github.com>
2026-03-05 13:05:02 +0530

    Time: 4 ms (21.71%) | Memory: 12.3 MB (85.27%) - LeetSync

--- 3429-special-array-i/special-array-i.py
+++ 3429-special-array-i/special-array-i.py
@@ -4,8 +4,8 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        for i in range(1, len(nums)):
-            if nums[i] % 2 == nums[i-1] % 2:
+        for i in range(1,len(nums)):
+            if nums[i]%2==nums[i-1]%2:
                 return False
         return True
         
