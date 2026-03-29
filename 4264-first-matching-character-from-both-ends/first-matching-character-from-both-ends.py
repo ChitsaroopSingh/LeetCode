8392f30e2fb3122397bc4d367417e6d36d735b91
ZeuS <69910617+ChitsaroopSingh@users.noreply.github.com>
2026-03-29 11:30:04 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (57.81%) - LeetSync

---
+++ 4264-first-matching-character-from-both-ends/first-matching-character-from-both-ends.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def firstMatchingIndex(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        s=list(s)
+        n=len(s)
+        for i in range(n):
+            if s[i]==s[n-i-1]:
+                return i
+        return -1
